6481f3b98d80bbea2c999def9f09d7c8183de61c
Mahmoud Abdelkader <mahmoud@balancedpayments.com>
2013-12-27 19:10:39 -0800

    Ok..its live :)

--- Berksfile
+++ Berksfile
@@ -4,5 +4,5 @@ site :opscode
 
 metadata
 
-
 cookbook 'devpi', github: 'dave-shawley/devpi-cookbook', ref: 'c61eda69484c82f218c11ee887b9f07f5c0f42a1'
+cookbook 'supervisor', github: 'poise/supervisor'
--- recipes/default.rb
+++ recipes/default.rb
@@ -22,4 +22,10 @@
 include_recipe 'balanced-apt'
 include_recipe 'devpi::server'
 include_recipe 'devpi::supervisor'
+
+directory '/var/log/devpi-server' do
+  action :create
+  recursive true
+end
+
 include_recipe 'devpi::nginx'
